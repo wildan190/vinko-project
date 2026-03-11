a7792e1077db1de64df3d637b65642c155e33ce2
Wildan Belfiore <mikewildan@outlook.com>
2026-03-12 00:28:49 +0700

    update

--- app/Jobs/MergeProductImageJob.php
+++ app/Jobs/MergeProductImageJob.php
@@ -59,46 +59,52 @@ class MergeProductImageJob implements ShouldQueue
         // 300 DPI conversion: 1 cm = 118.11 pixels
         $pxPerCm = 118.11;
 
-        // Requirement: Barcode box is fix 9cm x 2.5cm
-        $boxWidth = round(9 * $pxPerCm);   // ~1063px
-        $boxHeight = round(2.5 * $pxPerCm); // ~295px
+        // Scale factors: If image is very large, we scale up the header elements
+        // Base width for scaling is ~3300px (approx 28cm at 300 DPI)
+        $baseWidth = round(28 * $pxPerCm);
+        $scaleFactor = max(1, $imgWidth / $baseWidth);
 
-        // Requirement: Output height increases by ~3cm (e.g. 50cm -> 53cm)
-        // We will use 3cm as the total header area height
-        $headerAreaHeight = round(3 * $pxPerCm); // ~354px
+        // Requirement: Barcode box is fix 9cm x 2.5cm (base)
+        $boxWidth = round(9 * $pxPerCm * $scaleFactor);   
+        $boxHeight = round(2.5 * $pxPerCm * $scaleFactor); 
+
+        // Requirement: Output height increases by ~3cm (base)
+        $headerAreaHeight = round(3 * $pxPerCm * $scaleFactor); 
 
         // Create the Information Box (The "Barcode Box")
         $infoBox = $manager->create($boxWidth, $boxHeight)->fill('ffffff');
         
         // Add Border to Info Box
-        $infoBox->drawRectangle(0, 0, function($draw) use ($boxWidth, $boxHeight) {
+        $infoBox->drawRectangle(0, 0, function($draw) use ($boxWidth, $boxHeight, $scaleFactor) {
             $draw->size($boxWidth, $boxHeight);
-            $draw->border('000000', 2);
+            $draw->border('000000', max(2, round(2 * $scaleFactor)));
         });
 
         // Add QR Code (inside info box, right side)
         $qrContent = $this->product->tracking_number ?? $this->product->order_number ?? 'N/A';
+        $qrSize = round(240 * $scaleFactor);
         $qrCodeImage = QrCode::format('png')
-            ->size(240)
+            ->size($qrSize)
             ->margin(1)
             ->generate($qrContent);
         
         $qr = $manager->read((string) $qrCodeImage);
-        $infoBox->place($qr, 'right', 10);
+        $infoBox->place($qr, 'right', round(10 * $scaleFactor));
 
         // Font file
         $fontFile = '/System/Library/Fonts/Supplemental/Arial.ttf';
         $fontExists = file_exists($fontFile);
 
         // Add Text Info to Info Box (left side)
-        $infoBox->text("NO. PESANAN: " . $this->product->order_number, 15, 35, function($font) use ($fontExists, $fontFile) {
+        $paddingLeft = round(15 * $scaleFactor);
+        $infoBox->text("NO. PESANAN: " . $this->product->order_number, $paddingLeft, round(35 * $scaleFactor), function($font) use ($fontExists, $fontFile, $scaleFactor) {
             if ($fontExists) $font->file($fontFile);
-            $font->size(22);
+            $font->size(round(22 * $scaleFactor));
             $font->color('000000');
         });
-        $infoBox->text("SKU: " . $this->product->sku_platform, 15, 70, function($font) use ($fontExists, $fontFile) {
+        $infoBox->text("SKU: " . $this->product->sku_platform, $paddingLeft, round(70 * $scaleFactor), function($font) use ($fontExists, $fontFile, $scaleFactor) {
             if ($fontExists) $font->file($fontFile);
-            $font->size(22);
+            $font->size(round(22 * $scaleFactor));
             $font->color('000000');
         });
         
@@ -106,35 +112,36 @@ class MergeProductImageJob implements ShouldQueue
         $spec = $this->product->product_specification;
         if (strlen($spec) > 45) $spec = substr($spec, 0, 42) . '...';
         
-        $infoBox->text("SPEC: " . $spec, 15, 105, function($font) use ($fontExists, $fontFile) {
+        $infoBox->text("SPEC: " . $spec, $paddingLeft, round(105 * $scaleFactor), function($font) use ($fontExists, $fontFile, $scaleFactor) {
             if ($fontExists) $font->file($fontFile);
-            $font->size(18);
+            $font->size(round(18 * $scaleFactor));
             $font->color('000000');
         });
 
-        $infoBox->text("ID: " . $this->product->product_id, 15, 140, function($font) use ($fontExists, $fontFile) {
+        $infoBox->text("ID: " . $this->product->product_id, $paddingLeft, round(140 * $scaleFactor), function($font) use ($fontExists, $fontFile, $scaleFactor) {
             if ($fontExists) $font->file($fontFile);
-            $font->size(18);
+            $font->size(round(18 * $scaleFactor));
             $font->color('000000');
         });
 
-        $infoBox->text("Qty: " . $this->product->quantity, 15, 200, function($font) use ($fontExists, $fontFile) {
+        $infoBox->text("Qty: " . $this->product->quantity, $paddingLeft, round(200 * $scaleFactor), function($font) use ($fontExists, $fontFile, $scaleFactor) {
             if ($fontExists) $font->file($fontFile);
-            $font->size(45);
+            $font->size(round(45 * $scaleFactor));
             $font->color('000000');
         });
 
         // Create Final Canvas
-        // Width stays the same as product image, height increases by 3cm
+        // Width stays the same as product image, height increases by scaled 3cm
         $finalWidth = $imgWidth;
         $finalHeight = $imgHeight + $headerAreaHeight;
 
         $canvas = $manager->create($finalWidth, $finalHeight)->fill('ffffff');
 
         // Place Info Box in the header area
-        // If image is wider than 9cm, we can center it or put it on the left
-        // Based on "output jadi 28x53", we'll center the 9cm box in the 28cm header
-        $canvas->place($infoBox, 'top-center', 0, round(0.25 * $pxPerCm)); // 0.25cm padding from top
+        // Requirement: Align right at the top
+        $paddingRight = round(0.25 * $pxPerCm * $scaleFactor);
+        $paddingTop = round(0.25 * $pxPerCm * $scaleFactor);
+        $canvas->place($infoBox, 'top-right', $paddingRight, $paddingTop);
 
         // Place Main Product Image below the header area
         $canvas->place($mainImg, 'top-left', 0, $headerAreaHeight);
