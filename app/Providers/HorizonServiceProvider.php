658683ad163cc530786b1785990fe2cec09a4ca1
Wildan Belfiore <mikewildan@outlook.com>
2026-03-11 23:39:24 +0700

    update horizon

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -28,9 +28,8 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user = null) {
-            return in_array(optional($user)->email, [
-                //
-            ]);
+            // Dipaksa true agar kamu bisa masuk dan memantau antrian gambar
+            return true;
         });
     }
 }
