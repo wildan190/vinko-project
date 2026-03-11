57fffdcbdf21222f04663781c0ecd89a502fda26
Wildan Belfiore <mikewildan@outlook.com>
2026-03-12 00:22:16 +0700

    update

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -8,9 +8,7 @@ use Laravel\Horizon\HorizonApplicationServiceProvider;
 
 class HorizonServiceProvider extends HorizonApplicationServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     */
+     
     public function boot(): void
     {
         parent::boot();
